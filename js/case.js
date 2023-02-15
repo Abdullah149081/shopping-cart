cb1b5d6f8c65b6d88ae7d55722e3a23f2479f2b5
Abdullah149081 <masudmd425@gmail.com>
2023-02-15 10:26:52 +0600

    case add fX minusBtn

--- js/case.js
+++ js/case.js
@@ -1,3 +1,7 @@
 document.getElementById("btn-case-plus").addEventListener("click", function () {
   plusBtn("case-number-field");
 });
+
+document.getElementById("btn-case-minus").addEventListener("click", function () {
+  minusBtn("case-number-field");
+});
