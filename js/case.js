941a30cd35f31f4c0f0fe210334af6c337596f64
Abdullah149081 <masudmd425@gmail.com>
2023-02-15 13:45:23 +0600

    Return input field value and case current price multiplication

--- js/case.js
+++ js/case.js
@@ -1,7 +1,16 @@
 document.getElementById("btn-case-plus").addEventListener("click", function () {
-  plusBtn("case-number-field");
+  const caseBuy = plusBtn("case-number-field");
+  casePrice(caseBuy);
 });
 
 document.getElementById("btn-case-minus").addEventListener("click", function () {
-  minusBtn("case-number-field");
+  const caseBuy = minusBtn("case-number-field");
+  casePrice(caseBuy);
 });
+
+const caseCurrentAmount = stringToNumber("case-total");
+
+function casePrice(priceID) {
+  const priceValue = document.getElementById("case-total");
+  priceValue.innerText = priceID * caseCurrentAmount;
+}
